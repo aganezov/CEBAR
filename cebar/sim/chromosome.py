7941d13fa4e50707da3947db935a78696b01b090
aganezov <aganezov@gwu.edu>
2016-09-16 16:58:03 -0400

    basic start for chromosome structure

--- cebar/sim/chromosome.py
+++ cebar/sim/chromosome.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
+import enum
+
+
+class CHRTYPE(enum.Enum):
+    circular = 0
+    linear = 1
 
 
 class Chromosome(object):
-    pass
+    def __init__(self, name, chr_type=CHRTYPE.linear, blocks=None):
+        self.name = name
+        self.chr_type = chr_type
+        self.blocks = blocks if blocks is not None else []
